9d7aba25f7b6f6bbd3a61f75ecfd7342377d330a
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-05-24 20:41:56 +0400

    Create task 3.cs

---
+++ homework 3/task 3.cs
@@ -0,0 +1,15 @@
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "34";
+            int b = Convert.ToInt32(a);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
