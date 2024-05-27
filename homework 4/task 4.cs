1868a796c2d03518976d693ca16bb672ab8de9e6
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-05-27 12:12:32 +0400

    Create task 4.cs

---
+++ homework 4/task 4.cs
@@ -0,0 +1,16 @@
+namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Sunset whispers secrets; the ocean listens, stars dance, moonlight paints dreams, hearts connect.";
+            Console.WriteLine("Contains ocean: " + str.Contains("ocean"));
+            Console.WriteLine("Contains dance: " + str.Contains("dance"));
+            Console.WriteLine("Contains dreams: " + str.Contains("dreams"));
+            Console.WriteLine("Contains sunshine: " + str.Contains("sunshine"));
+            Console.WriteLine("Contains painting: " + str.Contains("painting"));
+            Console.ReadKey();
+        }
+    }
+}
