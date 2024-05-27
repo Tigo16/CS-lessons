16607e724ff970cc3f4c2e0b5eabbfe1d4ff0293
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-05-27 12:18:06 +0400

    Create task 7.cs

---
+++ homework 4/task 7.cs
@@ -0,0 +1,12 @@
+namespace task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "  Hello, World!  ";
+            Console.WriteLine(str.Trim());
+            Console.ReadKey();
+        }
+    }
+}
