525b6c59b3659d79ef9bbe09c0739b62b8259b02
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-05-27 12:16:49 +0400

    Create task 6.cs

---
+++ homework 4/task 6.cs
@@ -0,0 +1,13 @@
+namespace task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "I love programming in Java";
+            str = str.Replace("Java","C#");
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
