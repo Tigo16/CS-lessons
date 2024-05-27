76ea76b5606b6562303d9fccd37f957558078681
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-05-27 11:27:23 +0400

    Create task 1.cs

---
+++ homework 4/task 1.cs
@@ -0,0 +1,15 @@
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello, World!";
+            Console.WriteLine("String Lenght: " + str.Length);
+            Console.WriteLine("String Uppercase: " + str.ToUpper());
+            Console.WriteLine("String Lowercase:" + str.ToLower());
+            Console.WriteLine("Substring: " + str.Substring(7));
+	          Console.ReadKey();
+        }
+    }
+}
