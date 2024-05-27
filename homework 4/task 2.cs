7f464f1e4be11f7d76ca0be9ebdb37edb655eb88
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-05-27 11:28:39 +0400

    Create task 2.cs

---
+++ homework 4/task 2.cs
@@ -0,0 +1,16 @@
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "C#";
+            string str2 = "A";
+            string str3 = "to";
+            string str4 = "Z";
+            string res = str1 + ' ' + str2 + ' ' + str3 + ' ' + str4;
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
