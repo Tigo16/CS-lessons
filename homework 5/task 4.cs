3ce5d3eb6d95dd13199dec9598c70fcb8e64441e
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-04 17:13:30 +0400

    Create task 4.cs

---
+++ homework 5/task 4.cs
@@ -0,0 +1,39 @@
+namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint score = 77;
+            if (score >= 90)
+            {
+                Console.WriteLine("It's A");
+            }
+            else
+            {
+                if (score >= 80)
+                {
+                    Console.WriteLine("It's B");
+                }
+                else
+                {
+                    if(score >= 70)
+                    {
+                        Console.WriteLine("It's C");
+                    }
+                    else
+                    {
+                        if (score >= 60) {
+                            Console.WriteLine("It's D");
+                        }
+                        else
+                        {
+                            Console.WriteLine("It's F");
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
