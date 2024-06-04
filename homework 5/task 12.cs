bcfc72b3da8d3583c34a522cee38505ceacdd59a
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-04 19:06:25 +0400

    Create task 12.cs

---
+++ homework 5/task 12.cs
@@ -0,0 +1,24 @@
+namespace task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = -58;
+            if (num > 0) {
+                Console.WriteLine("The number is positive");
+            }
+            else
+            {
+                if (num < 0) {
+                    Console.WriteLine("The number is negative");
+                }
+                else
+                {
+                    Console.WriteLine("The number is zero");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
