fdc97c0b49dcc441b2a216f857f324962bbbc561
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-04 16:52:47 +0400

    Create task 2.cs

---
+++ homework 5/task 2.cs
@@ -0,0 +1,19 @@
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 29;
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+            Console.ReadKey();
+        }
+    }
+}
