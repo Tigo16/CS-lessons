fcae6d7b84c720aacb6b7b63e702a909e5ecdf20
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-04 16:46:58 +0400

    Create task 1.cs

---
+++ homework 5/task 1.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12;
+            if (number > 10)
+            {
+                Console.WriteLine("The number is greater than 10.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
