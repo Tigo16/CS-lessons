03da0a52d88698cd1a19fb63c4641243de8fc3ec
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-06 17:53:50 +0400

    Update task 2.cs

--- homework 6/task 2.cs
+++ homework 6/task 2.cs
@@ -4,7 +4,7 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            int n;
+            uint n;
             int sum = 0;
             n = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i <= n; i++)
