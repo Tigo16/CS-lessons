7c03180279ab9601d0acfe8dde36f6980381d655
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-06 17:29:22 +0400

    Create task 1.cs

---
+++ homework 6/task 1.cs
@@ -0,0 +1,12 @@
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
