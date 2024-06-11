86676420af12f58e72c74da7e304571546daf9b9
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-11 16:38:54 +0400

    Create task 9.cs

---
+++ homework 7/task 9.cs
@@ -0,0 +1,13 @@
+namespace task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            str = Console.ReadLine();
+            string[] parts = str.Split(' ');
+            Console.WriteLine(parts.Length);
+        }
+    }
+}
