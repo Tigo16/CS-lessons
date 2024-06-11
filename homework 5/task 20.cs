7bf7d8d9e14e0b2f63409a239e28fce5fb02ab86
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-11 14:46:37 +0400

    Create task 20.cs

---
+++ homework 5/task 20.cs
@@ -0,0 +1,144 @@
+namespace task20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 147;
+            int n2 = 193;
+            int n3 = 74;
+            int n4 = 160;
+            int n5 = 111;
+            if (n1 > n2)
+            {
+                if (n1 > n3)
+                {
+                    if (n1 > n4)
+                    {
+                        if (n1 > n4)
+                        {
+                            Console.WriteLine("Max number: " + n1);
+                        }
+                    }
+                }
+            }
+            if (n2 > n1)
+            {
+                if (n2 > n3)
+                {
+                    if (n2 > n4)
+                    {
+                        if (n2 > n5)
+                        {
+                            Console.WriteLine("Max number: " + n2);
+                        }
+                    }
+                }
+            }
+            if (n3 > n1)
+            {
+                if (n3 > n2)
+                {
+                    if (n3 > n4)
+                    {
+                        if (n3 > n5)
+                        {
+                            Console.WriteLine("Max number: " + n3);
+                        }
+                    }
+                }
+            }
+            if (n4 > n1)
+            {
+                if (n4 > n2)
+                {
+                    if (n4 > n3)
+                    {
+                        if (n4 > n5)
+                        {
+                            Console.WriteLine("Max number: " + n4);
+                        }
+                    }
+                }
+            }
+            if (n5 > n1)
+            {
+                if (n5 > n2)
+                {
+                    if (n5 > n3)
+                    {
+                        if (n5 > n4)
+                        {
+                            Console.WriteLine("Max number: " + n5);
+                        }
+                    }
+                }
+            }
+            if (n1 < n2)
+            {
+                if (n1 < n3)
+                {
+                    if (n1 < n4)
+                    {
+                        if (n1 < n4)
+                        {
+                            Console.WriteLine("Min number: " + n1);
+                        }
+                    }
+                }
+            }
+            if (n2 < n1)
+            {
+                if (n2 < n3)
+                {
+                    if (n2 < n4)
+                    {
+                        if (n2 < n5)
+                        {
+                            Console.WriteLine("Min number:" + n2);
+                        }
+                    }
+                }
+            }
+            if (n3 < n1)
+            {
+                if (n3 < n2)
+                {
+                    if (n3 < n4)
+                    {
+                        if (n3 < n5)
+                        {
+                            Console.WriteLine("Min number: " + n3);
+                        }
+                    }
+                }
+            }
+            if (n4 < n1)
+            {
+                if (n4 < n2)
+                {
+                    if (n4 < n3)
+                    {
+                        if (n4 < n5)
+                        {
+                            Console.WriteLine("Min number: " + n4);
+                        }
+                    }
+                }
+            }
+            if (n5 < n1)
+            {
+                if (n5 < n2)
+                {
+                    if (n5 < n3)
+                    {
+                        if (n5 < n4)
+                        {
+                            Console.WriteLine("Min number: " + n5);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
