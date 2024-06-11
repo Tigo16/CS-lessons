cb1e3064822f8d5e12b487c8a8c216d3065bde2d
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-11 16:31:28 +0400

    Create task 8.cs

---
+++ homework 7/task 8.cs
@@ -0,0 +1,14 @@
+namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 100));
+            }
+        }
+    }
+}
