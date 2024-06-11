1d253d6ce0f64607552dd5386c9a94df7be3e38d
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-11 14:34:26 +0400

    Create task 19.cs

---
+++ homework 5/task 19.cs
@@ -0,0 +1,24 @@
+namespace task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol;
+            symbol = Convert.ToChar(Console.ReadLine());
+            switch (symbol)
+            {
+                case 'a': 
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("Symbol is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("Symbol is a consonant");
+                    break;
+            }
+        }
+    }
+}
