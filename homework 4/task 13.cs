f63e6c09f5c868cdb69e9c40e1a6ec22efa5e0d5
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-13 11:38:30 +0400

    Create task 13.cs

---
+++ homework 4/task 13.cs
@@ -0,0 +1,15 @@
+namespace task13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "hello";
+            string str2 = "hello";
+            string str3 = "world";
+
+            Console.WriteLine(str1 == str2);
+            Console.WriteLine(str1 == str3);
+        }
+    }
+}
