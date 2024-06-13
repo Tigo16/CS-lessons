00148fec818a46dc5a0cb8fc7591fb50e09e12b2
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-06-13 11:37:20 +0400

    Create task 12.cs

---
+++ homework 4/task 12.cs
@@ -0,0 +1,15 @@
+namespace task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "hello";
+            string str2 = "hello";
+            string str3 = "world";
+
+            Console.WriteLine(str1.Equals(str2));
+            Console.WriteLine(str1.Equals(str3));
+        }
+    }
+}
