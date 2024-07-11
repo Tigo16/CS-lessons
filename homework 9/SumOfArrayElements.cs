24807be05fe24231148896fd996bf92e657118d4
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-07-11 14:29:11 +0400

    Update SumOfArrayElements.cs

--- homework 9/SumOfArrayElements.cs
+++ homework 9/SumOfArrayElements.cs
@@ -6,12 +6,14 @@ namespace Sum_of_Array_Elements
         {
             Array array = Array.CreateInstance(typeof(int), 3);
             int sum = 0;
+            
             Console.WriteLine("Input 3 elements in the array");
             for (int i = 0; i < array.Length; i++)
             {
                 array.SetValue(int.Parse(Console.ReadLine()), i);
                 sum += (int)array.GetValue(i);
             }
+            
             Console.Write($"Sum of all elements stored in the array is: {sum}");
         }
     }
