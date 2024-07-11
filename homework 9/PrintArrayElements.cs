cc491a9a75f15ef231acfe9d51d21e36d0f73f5e
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-07-11 14:27:59 +0400

    Update PrintArrayElements.cs

--- homework 9/PrintArrayElements.cs
+++ homework 9/PrintArrayElements.cs
@@ -5,11 +5,13 @@ namespace Print_Array_Elements
         static void Main(string[] args)
         {
             Array array = Array.CreateInstance(typeof(int), 10);
+            
             Console.WriteLine("Input 10 elements in the array");
             for (int i = 0; i < array.Length; i++)
             {
                 array.SetValue(int.Parse(Console.ReadLine()), i);
             }
+            
             Console.Write("Elements in the array are: ");
             for(int i = 0; i < array.Length; i++)
             {
