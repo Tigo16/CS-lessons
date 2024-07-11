b715e0f20a67a0f0cfd18981e122e620a1fe6e43
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-07-11 14:09:33 +0400

    Update ReverseOrder.cs

--- homework 9/ReverseOrder.cs
+++ homework 9/ReverseOrder.cs
@@ -4,25 +4,25 @@ namespace Reverse_Order
     {
         static void Main(string[] args)
         {
-            int n;
             Console.WriteLine("Input the number of elements to store in the array");
-            n = Convert.ToInt32(Console.ReadLine());
-            int[] numbers = new int[n];
+            int n = int.Parse(Console.ReadLine());
+            Array array = Array.CreateInstance(typeof(int), n);
             Console.WriteLine($"Input {n} elements in the array");
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                array.SetValue(int.Parse(Console.ReadLine()), i);
             }
             Console.Write("The values stored in the array are: ");
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(numbers[i] + " ");
+                Console.Write(array.GetValue(i) + " "); 
             }
             Console.WriteLine();
+            Array.Reverse(array);
             Console.Write("The values in reverse order are: ");
-            for (int i = numbers.Length - 1; i >= 0; i--)
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(numbers[i] + " ");
+                Console.Write(array.GetValue(i) + " ");
             }
         }
     }
