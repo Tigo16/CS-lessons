286ca9b7ea638339d5d7b4487c93b0041847f311
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-07-11 14:27:35 +0400

    Update CopyElementsToAnotherArray.cs

--- homework 9/CopyElementsToAnotherArray.cs
+++ homework 9/CopyElementsToAnotherArray.cs
@@ -6,11 +6,13 @@ namespace Copy_Elements_to_Another_Array
         {
             Array array1 = Array.CreateInstance(typeof(int), 3);
             Array array2 = Array.CreateInstance(typeof(int), 3);
+            
             Console.WriteLine("Input 3 elements in the array");
             for (int i = 0; i < array1.Length; i++)
             {
                 array1.SetValue(int.Parse(Console.ReadLine()), i);
             }
+            
             Array.Copy(array1, array2, array1.Length);
 
             Console.Write("Elements stored in the first array: ");
