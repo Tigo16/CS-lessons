e6261a288c1ee75778ca2c16d79a5d23314785f4
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2024-07-11 13:42:46 +0400

    Update and rename task 1 to task 1.cs

--- homework 9/task 1.cs
+++ homework 9/task 1.cs
@@ -4,7 +4,12 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[10] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] numbers = new int[10];
+            Console.WriteLine("Input 10 elements in the array");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
             Console.Write("Elements in the array are: ");
             for (int i = 0; i < numbers.Length; i++)
             {
